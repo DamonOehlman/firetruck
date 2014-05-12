733e8b84203335bf892016d1ecabd2366b3d9365
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-12 17:11:46 +1000

    Handle functions provided as results

--- builder.js
+++ builder.js
@@ -2,6 +2,17 @@ module.exports = function(callback) {
   return {
     // json handler
     json: function(obj) {
+      // if we've been provided a function, then we need to do some of the dirty work
+      if (typeof obj == 'function') {
+        return callback(null, function(req, res) {
+          res.writeHead(200, {
+            'Content-Type': 'application/json'
+          });
+
+          obj(req, res);
+        });
+      }
+
       callback(null, function(req, res) {
         res.writeHead(200, {
           'Content-Type': 'application/json'
@@ -12,6 +23,16 @@ module.exports = function(callback) {
 
     // text handler
     text: function(text) {
+      if (typeof text == 'function') {
+        return callback(null, function(req, res) {
+          res.writeHead(200, {
+            'Content-Type': 'text/plain'
+          });
+
+          text(req, res);
+        });
+      }
+
       callback(null, function(req, res) {
         res.writeHead(200, {
           'Content-Type': 'text/plain'
