775bae903e0cb3a06af56c766084b771663efa24
Damon Oehlman <damon.oehlman@gmail.com>
2014-07-30 17:08:59 +1000

    Write a heartbeat to ensure the SSE channel stays open

--- sse.js
+++ sse.js
@@ -1,8 +1,23 @@
 var reLineBreak = /\n\r?/g;
 
 module.exports = function(res) {
+  var heartbeatTimer;
+
+  function resetHeartbeat() {
+    clearTimeout(heartbeatTimer);
+
+    heartbeatTimer = setTimeout(function() {
+      res.write(':hb\n\n');
+
+      // queue another heartbeat write
+      heartbeatTimer = resetHeartbeat();
+    }, 5000);
+  }
+
   function writer(type, payload) {
     function write(data) {
+      resetHeartbeat();
+
       if (type) {
         res.write('event: ' + type + '\n');
       }
@@ -15,5 +30,6 @@ module.exports = function(res) {
 
   writer.close = res.end.bind(res);
 
+  resetHeartbeat();
   return writer;
 };
