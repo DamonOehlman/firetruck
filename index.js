70cfe51dbe082bd7a2c21ebdc263278860b575c6
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-24 12:13:02 +1100

    Disable caching in st

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ var firetruck = module.exports = function(server, opts) {
     }
 
     // mount the static serving endpoint
-    mount = st({ path: path.resolve(clientPath, 'static'), index: 'index.html' });
+    mount = st({ path: path.resolve(clientPath, 'static'), cache: false, index: 'index.html' });
 
     app.browserify(path.resolve(clientPath, 'js'));
     router.define('/*', mount);
