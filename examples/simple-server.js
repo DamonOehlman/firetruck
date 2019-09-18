3fe0ede5ec5e4ecbb3b47a316c05226a2cc23575
Damon Oehlman <damon.oehlman@gmail.com>
2019-09-18 21:43:03 +1000

    upgrade packages, add simple test route to the demo server

--- examples/simple-server.js
+++ examples/simple-server.js
@@ -1,5 +1,11 @@
 var server = require('http').createServer();
 var app = require('..')(server);
 
+app('/test', function(req) {
+  this.ok();
+  this.write('test');
+  this.end();
+});
+
 // listen on port 3000
 server.listen(3000);
